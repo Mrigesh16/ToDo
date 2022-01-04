3ffc28372246a67efcaf2687805b64395a36f4ed
Mrigesh <006mrigesh@gmail.com>
2022-01-04 16:51:44 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <NavBar/>
+  <router-view/>
+</template>
+
+<script>
+import NavBar from '../src/components/NavBar.vue'
+
+export default {
+  components:{NavBar}
+}
+</script>
+
+
+<style>
+body{
+  background: #f2f2f2;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 0 auto;
+  max-width: 600px;
+  color: #555;
+}
+</style>
